ee5a64cb99b044258a32c2255ba9ddc84de6b36e
dev_kong <ahdjd5@gmail.com>
2022-03-24 16:40:58 +0900

    [front/qa] qa list html/css done

--- front/routes/qa/qa.controller.js
+++ front/routes/qa/qa.controller.js
@@ -1,3 +1,7 @@
 exports.write = (req, res) => {
   res.render('qa/write.html');
 };
+
+exports.list = (req, res) => {
+  res.render('qa/list.html');
+};
--- front/routes/qa/qaRouter.js
+++ front/routes/qa/qaRouter.js
@@ -4,6 +4,8 @@ const qaController = require('./qa.controller.js');
 
 const router = express.Router();
 
+router.get('/list', qaController.list);
+
 router.get('/write', qaController.write);
 
 module.exports = router;
