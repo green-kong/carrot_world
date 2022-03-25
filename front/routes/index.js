4af80618515ed302714dfcd401306d9a2ab67b45
dev_kong <ahdjd5@gmail.com>
2022-03-25 11:19:18 +0900

    [front/chat] chat nunjucks setting done

--- front/routes/index.js
+++ front/routes/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const adminRouter = require('./admin/adminRouter.js');
 const qaRouter = require('./qa/qaRouter.js');
+const chatRouter = require('./chat/chatRouter.js');
 
 const router = express.Router();
 
 router.use('/admin', adminRouter);
 
 router.use('/qa', qaRouter);
+
+router.use('/chat', chatRouter);
 
 module.exports = router;
