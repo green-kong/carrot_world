88c7f2a5f43c394a47f8f9ffc9ffd9d521a358b9
dev_kong <ahdjd5@gmail.com>
2022-03-27 01:57:28 +0900

    [back/home]wip: writing sql

--- back/routes/home/home.controller.js
+++ back/routes/home/home.controller.js
@@ -1,4 +1,75 @@
-exports.write = (req, res) => {
-  console.log(req.files);
-  console.log(req.body);
+const { pool } = require('../../model/db/db.js');
+
+exports.write = async (req, res) => {
+  const {
+    subject,
+    dealWay,
+    bidDate,
+    productType,
+    dealPrice,
+    dealZone,
+    productDetail,
+    tag,
+  } = req.body;
+
+  const tagList = tag
+    .replace(/(\s*)/g, '')
+    .split('#')
+    .filter((v) => v !== '');
+
+  const fileList = req.files.map((v) => v.filename);
+
+  const conn = await pool.getConnection();
+  try {
+    let sql;
+    let boardtype;
+    let prepare;
+    if (dealWay === 'sell') {
+      boardtype = 's';
+      sql = `INSERT INTO sell_board(
+             c_code,subject,u_id,price,content,location,date
+             )VALUES(
+             ?,?,?,?,?,?,now()
+             )`;
+      prepare = [productType, subject, 1, dealPrice, productDetail, dealZone];
+    } else {
+      const bidStart = new Date();
+      bidStart.setDate(bidStart.getDate() + bidDate);
+      boardtype = 'au';
+      sql = `INSERT INTO auction(
+             c_code,subject,u_id,price,content,location,date,startDate
+             )VALUES(
+             ?,?,?,?,?,?,now(),?
+             )`;
+      prepare = [];
+    }
+    const [result] = await conn.query(sql, prepare);
+    const idx = result.insertId;
+
+    tagList.forEach(async (v) => {
+      const tagSql = `INSERT INTO ${boardtype}_tag
+                        (${boardtype}_id,tag)
+                        VALUES
+                        ('${idx}','${v}')`;
+      await conn.query(tagSql);
+    });
+
+    fileList.forEach(async (v) => {
+      const imgSql = `INSERT INTO ${boardtype}_img
+                        (${boardtype}_id,img)
+                        VALUES
+                        ('${idx}','${v}')`;
+      await conn.query(imgSql);
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    conn.release();
+  }
 };
+
+const bidStart = new Date();
+bidStart.setDate(bidStart.getDate() + 1);
+const y = bidStart.getFullYear();
+const m = bidStart.getMonth();
+const d = bidStart.getDate();
