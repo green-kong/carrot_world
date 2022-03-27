3de6f81ee5346e3c3680334dc95ac8afe354677c
dev_kong <ahdjd5@gmail.com>
2022-03-27 17:50:52 +0900

    [front/home]feat: make category list with data from back srv

--- front/public/js/home/main.js
+++ front/public/js/home/main.js
@@ -1,6 +1,21 @@
 async function init() {
   const url = 'http://localhost:4000/api/home/';
-  const categoryList = await axios.post(url);
+  const response = await axios.post(url);
+  if (response.status === 200) {
+    const categoryTem = document.querySelector('#category_tem').innerHTML;
+    const sellList = document.querySelector('#sell_list');
+    const bidList = document.querySelector('#bid_list');
+    const categoryList = response.data.reduce((acc, cur) => {
+      return (
+        acc +
+        categoryTem
+          .replace('{c_code}', cur.c_code)
+          .replace('{c_name}', cur.c_name)
+      );
+    }, '');
+    sellList.innerHTML = categoryList;
+    bidList.innerHTML = categoryList;
+  }
 }
 
 init();
