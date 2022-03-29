7420145d75bdc7bb71d626aafaa46c4ff6f4c350
dev_kong <ahdjd5@gmail.com>
2022-03-30 00:26:34 +0900

    [front/home]view sell board done

--- front/public/js/home/draw_page/drawView.js
+++ front/public/js/home/draw_page/drawView.js
@@ -6,13 +6,16 @@ export default async function drawView() {
 
   const response = await axios.post(url, body);
   if (response.status === 200) {
-    const { imgList, itemResult, tagList } = response.data;
+    const { imgList, itemResult, tagList, recList } = response.data;
 
     const contentFrame = document.querySelector('#content_frame');
 
     const viewTemp = document.querySelector('#view_template').innerHTML;
     const viewImgTemp = document.querySelector('#view_img_template').innerHTML;
     const viewTagTemp = document.querySelector('#view_tag_template').innerHTML;
+    const viewRecTemp = document.querySelector(
+      '#view_recommend_template'
+    ).innerHTML;
 
     const imgResult = imgList.reduce((acc, cur) => {
       return acc + viewImgTemp.replace('{img}', cur.img);
@@ -22,18 +25,34 @@ export default async function drawView() {
       return acc + viewTagTemp.replace('{tag}', cur.tag);
     }, '');
 
+    let recResult = '연관된 물품이 없습니다.';
+
+    if (recList !== undefined) {
+      recResult = recList.reduce((acc, cur) => {
+        return (
+          acc +
+          viewRecTemp
+            .replace('{table}', table)
+            .replace('{s_id}', cur.s_id)
+            .replace('{img}', cur.img)
+            .replace('{subject}', cur.subject)
+            .replace('{price}', cur.price)
+        );
+      }, '');
+    }
+
     const result = viewTemp
       .replace('{imgList}', imgResult)
       .replace('{subject}', itemResult.subject)
       .replace('{price}', itemResult.price)
-      .replace('{like}', itemResult.like)
+      .replace('{like}', itemResult.likes)
       .replace('{date}', itemResult.date)
       .replace('{how}', itemResult.how)
       .replace('{location}', itemResult.location)
       .replace('{tagList}', tagResult)
-      .replace('{content}', itemResult.content);
-
-    console.log(result);
+      .replace('{content}', itemResult.content)
+      .replace('{c_name}', itemResult.c_name)
+      .replace('{recommendList}', recResult);
 
     contentFrame.innerHTML = result;
   }
