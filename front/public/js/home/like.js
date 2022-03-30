198d7627535bbe474f0a9b649343dff72b9d4372
dev_kong <ahdjd5@gmail.com>
2022-03-31 01:19:19 +0900

    [front/like]wip: front axios

--- front/public/js/home/like.js
+++ front/public/js/home/like.js
@@ -1,5 +1,11 @@
-function likeClickHandler() {
-  console.log('like btn!!');
+async function likeClickHandler() {
+  const [, table, itemIdx] = window.location.hash.replace('#', '').split('/');
+  const userIdx = document.querySelector('#u_id').value;
+
+  const url = 'http://localhost:4000/api/home/like';
+  const body = { table, itemIdx, userIdx };
+
+  const response = axios.post(url, body);
 }
 
 export default async function activeLike() {
