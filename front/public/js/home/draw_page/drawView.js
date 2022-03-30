89178c7c8f577cfef7473fffe1a6d48faed37c4e
dev_kong <ahdjd5@gmail.com>
2022-03-30 16:34:43 +0900

    [front/home]fix : auction view render done

--- front/public/js/home/draw_page/drawView.js
+++ front/public/js/home/draw_page/drawView.js
@@ -16,6 +16,9 @@ export default async function drawView() {
     const viewRecTemp = document.querySelector(
       '#view_recommend_template'
     ).innerHTML;
+    const auviewRecTemp = document.querySelector(
+      '#au_view_recommend_template'
+    ).innerHTML;
 
     const imgResult = imgList.reduce((acc, cur) => {
       return acc + viewImgTemp.replace('{img}', cur.img);
@@ -26,34 +29,62 @@ export default async function drawView() {
     }, '');
 
     let recResult = '연관된 물품이 없습니다.';
+    let result;
+    if (table === 'sell_board') {
+      if (recList !== undefined) {
+        recResult = recList.reduce((acc, cur) => {
+          return (
+            acc +
+            viewRecTemp
+              .replace('{table}', table)
+              .replace('{s_id}', cur.s_id)
+              .replace('{img}', cur.img)
+              .replace('{subject}', cur.subject)
+              .replace('{price}', cur.price)
+          );
+        }, '');
+      }
 
-    if (recList !== undefined) {
-      recResult = recList.reduce((acc, cur) => {
-        return (
-          acc +
-          viewRecTemp
-            .replace('{table}', table)
-            .replace('{s_id}', cur.s_id)
-            .replace('{img}', cur.img)
-            .replace('{subject}', cur.subject)
-            .replace('{price}', cur.price)
-        );
-      }, '');
+      result = viewTemp
+        .replace('{imgList}', imgResult)
+        .replace('{subject}', itemResult.subject)
+        .replace('{price}', itemResult.price)
+        .replace('{like}', itemResult.likes)
+        .replace('{date}', itemResult.date)
+        .replace('{how}', itemResult.how)
+        .replace('{location}', itemResult.location)
+        .replace('{tagList}', tagResult)
+        .replace('{content}', itemResult.content)
+        .replace('{c_name}', itemResult.c_name)
+        .replace('{recommendList}', recResult);
+    } else {
+      if (recList !== undefined) {
+        recResult = recList.reduce((acc, cur) => {
+          return (
+            acc +
+            auviewRecTemp
+              .replace('{table}', table)
+              .replace('{au_id}', cur.au_id)
+              .replace('{img}', cur.img)
+              .replace('{subject}', cur.subject)
+              .replace('{price}', cur.price)
+          );
+        }, '');
+      }
+
+      result = viewTemp
+        .replace('{imgList}', imgResult)
+        .replace('{subject}', itemResult.subject)
+        .replace('{price}', itemResult.price)
+        .replace('{like}', itemResult.likes)
+        .replace('{date}', itemResult.date)
+        .replace('{how}', itemResult.how)
+        .replace('{location}', itemResult.location)
+        .replace('{tagList}', tagResult)
+        .replace('{content}', itemResult.content)
+        .replace('{c_name}', itemResult.c_name)
+        .replace('{recommendList}', recResult);
     }
-
-    const result = viewTemp
-      .replace('{imgList}', imgResult)
-      .replace('{subject}', itemResult.subject)
-      .replace('{price}', itemResult.price)
-      .replace('{like}', itemResult.likes)
-      .replace('{date}', itemResult.date)
-      .replace('{how}', itemResult.how)
-      .replace('{location}', itemResult.location)
-      .replace('{tagList}', tagResult)
-      .replace('{content}', itemResult.content)
-      .replace('{c_name}', itemResult.c_name)
-      .replace('{recommendList}', recResult);
-
     contentFrame.innerHTML = result;
   }
 }
