bce526775580e6fee83c7ffc4bea956c4fd1c313
LeeHwaSeob <philar_@naver.com>
2022-03-31 12:30:01 +0900

    [front/user] wip: add form tag in home.html logout

--- front/routes/user/userRouter.js
+++ front/routes/user/userRouter.js
@@ -5,6 +5,7 @@ const userControll = require('./user.controller.js');
 const router = express.Router();
 
 const unauth = require('../../middlewares/user/unauth.js');
+const alertmove = require('../../utils/user/alertmove.js');
 
 function loginMiddleware(req, res) {
   res.render('user/login.html');
@@ -14,6 +15,10 @@ router.get('/login', unauth, loginMiddleware);
 
 router.get('/join', (req, res) => {
   res.render('/join.html');
+});
+
+router.get('/logout', (req, res) => {
+  res.send(alertmove('http://localhost:3000', '로그아웃 되었습니다.'));
 });
 
 router.get('/profile', userControll.profile);
