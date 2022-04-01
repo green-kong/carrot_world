a672301dac330454060f45f67cb42151e993f4b0
dev_kong <ahdjd5@gmail.com>
2022-04-01 16:33:16 +0900

    [front/home]fix: when reload page, moves home main

--- front/public/js/home/index.js
+++ front/public/js/home/index.js
@@ -31,4 +31,5 @@ function router() {
 }
 
 window.addEventListener('hashchange', router);
+window.addEventListener('DOMContentLoaded', router);
 window.addEventListener('hashchange', auctionSocket);
