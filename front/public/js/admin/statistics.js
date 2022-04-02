9455bd6e5cb17ca773455b84bbfa81d0a877c1c5
dev_kong <ahdjd5@gmail.com>
2022-04-03 02:04:16 +0900

    [front/statistics]feat: graph done

--- front/public/js/admin/statistics.js
+++ front/public/js/admin/statistics.js
@@ -1,20 +1,31 @@
 (async function () {
+  const START_COLOR = 90;
+  const END_COLOR = 20;
+
+  const colorList = [];
+
   const context = document.getElementById('myChart').getContext('2d');
 
   const url = 'http://localhost:4000/api/statistics/graph';
 
   const response = await axios.post(url);
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+  const labels = response.data.map((v) => v[0]);
 
+  const colors = labels.map((v, i, t) => {
+    const avg = (START_COLOR - END_COLOR) / (t.length - 1);
+    return `hsl(269, 89%, ${START_COLOR - avg * i}%)`;
+  });
+  // 269/89
+  console.log(colors);
   const data = {
     labels,
     datasets: [
       {
         label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
+        backgroundColor: colors,
         borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
+        data: response.data.map((v) => v[1]),
       },
     ],
   };
@@ -22,7 +33,13 @@
   const config = {
     type: 'bar',
     data,
-    options: {},
+    options: {
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
   };
 
   const myChart = new Chart(context, config);
