346306856f973f2c030aa43fb412dd268c0064af
yjleeinkr <lucy.lee930216@gmail.com>
2022-04-04 20:34:56 +0900

    [full/chat] feat : done for showing chatList on the left side

--- back/routes/chat/chatController.js
+++ back/routes/chat/chatController.js
@@ -1,56 +1,36 @@
 const { pool } = require('../../model/db/db.js');
 
 exports.chat = async (req, res) => {
-  const { u_id, userAlias, u_img, chatResult } = req.body;
-  console.log(u_id, userAlias, u_img, chatResult);
+  const { u_id, chatResult } = req.body;
   const conn = await pool.getConnection();
+  const chatList = chatResult.join(',');
   try {
-    const bringSql = `SELECT ANY_VALUE(chat.c_id), ANY_VALUE(chat.mem1), ANY_VALUE(chat.mem2), 
-                      ANY_VALUE(u1.userAlias) AS mem1Alias, 
-                      ANY_VALUE(u2.userAlias) AS mem2Alias, 
-                      ANY_VALUE(c1.date) AS mem1date, 
-                      (SELECT date FROM chat_log WHERE c_id )
-                      
-                      FROM 
-                        (SELECT c_id, 
-                          SUBSTRING_INDEX(members,',',1) AS mem1, 
-                          SUBSTRING_INDEX(members,',',-1) AS mem2 
-                          FROM chat) chat
+    const bringSql = `SELECT chat.c_id, chat.mem1, chat.mem2, 
+                      u1.userAlias AS mem1Alias, 
+                      u2.userAlias AS mem2Alias,
+                      u1.u_img AS mem1Img,
+                      u2.u_img AS mem2Img,
+                      chat.lastDate, chat.lastMsg 
+                      FROM chat 
                       JOIN user u1 
                       ON mem1 = u1.u_id 
                       JOIN user u2 
                       ON mem2 = u2.u_id 
-                      WHERE chat.c_id IN (1,2,3)
-                          
-                        (${chatResult})
-                      `;
-
-    `SELECT chat.c_id, chat.mem1, chat.mem2, 
-                      u1.userAlias AS mem1Alias, 
-                      u2.userAlias AS mem2Alias, 
-                      c1.date AS mem1date, 
-                      c2.date AS mem2date, 
-                      c1.dialog AS mem1dialog, 
-                      c2.dialog AS mem2dialog 
-                      FROM 
-                        (SELECT c_id, 
-                          SUBSTRING_INDEX(members,',',1) AS mem1, 
-                          SUBSTRING_INDEX(members,',',-1) AS mem2 
-                          FROM chat) chat
-                          JOIN user u1 
-                          ON mem1 = u1.u_id 
-                          JOIN user u2 
-                          ON mem2 = u2.u_id 
-                          JOIN chat_log c1 
-                          ON mem1 = c1.u_id 
-                          JOIN chat_log c2 
-                          ON mem2 = c2.u_id 
-                          WHERE chat.c_id IN (1,2,3)
-                          GROUP BY chat
-                          `;
-    const [chatLog] = conn.query(sql);
+                      WHERE chat.c_id IN (${chatList})`;
+    const [result] = await conn.query(bringSql);
+    const newResult = result.map((v) => {
+      const obj = {};
+      obj.c_id = v.c_id;
+      obj.theOther = v.mem1 === u_id ? v.mem2Alias : v.mem1Alias;
+      obj.thumbnail = v.mem1 === u_id ? v.mem2Img : v.mem1Img;
+      obj.lastDate = v.lastDate;
+      obj.lastMsg = v.lastMsg;
+      return obj;
+    });
+    res.send(newResult);
   } catch (err) {
     console.log(err.message);
+    res.status(500).send('fail');
   } finally {
     conn.release();
   }
--- front/routes/chat/chat.controller.js
+++ front/routes/chat/chat.controller.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
 
 exports.chat = async (req, res) => {
-  const { u_id, userAlias, u_img } = req.user.userResult;
+  const { u_id } = req.user.userResult;
   const { chatResult } = req.user;
-  const body = {
-    u_id,
-    userAlias,
-    u_img,
-    chatResult,
-  };
+  const body = { u_id, chatResult };
   const url = `http://localhost:4000/api/chat`;
   try {
     const response = await axios.post(url, body);
     console.log(response.data);
+    const { data } = response;
+    data.forEach((v) => {
+      if (v.thumbnail === null) v.thumbnail = `/img/carrotForNull.png`;
+      if (v.lastDate === null) v.lastDate = `no date`;
+      if (v.lastMsg === null) v.lastMsg = `내용 없음`;
+    });
+    res.render('chat/chat.html', { chatList: data });
   } catch (err) {
     console.log(err.message);
   }
-
-  res.render('chat/chat.html');
 };
