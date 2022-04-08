a59e2575e03649e554bb763c22ac7ce3a190c405
dev_kong <ahdjd5@gmail.com>
2022-04-08 16:50:39 +0900

    [front/admin]style: admin controller delete chonsole

--- front/public/js/admin/adminProfile.js
+++ front/public/js/admin/adminProfile.js
@@ -2,6 +2,5 @@ const userProfile = document.querySelector('#picture');
 const uploadName = document.querySelector('.upload_name');
 
 userProfile.addEventListener('change', (e) => {
-  console.log(e.target.value);
   uploadName.placeholder = e.target.value;
 });
